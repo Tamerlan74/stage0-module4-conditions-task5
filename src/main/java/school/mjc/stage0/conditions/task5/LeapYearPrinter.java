4e9597832edc43cf5bdb921833d5e3297f6cadbb
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-08-26 19:40:40 +0600

    ft

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.time.LocalDate;
+
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        System.out.println(LocalDate.of(year, 1,1).isLeapYear()?"leap":"not leap");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
-    }
+        switch (month) {
+            case 2:
+                System.out.println(28);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            case 1,3,5,7,8,10,12:
+                System.out.println(31);
+                break;
+            default:
+                System.out.println("wrong number!");
+        }    }
 }
